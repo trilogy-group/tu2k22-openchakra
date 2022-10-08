ad82413b226ac2807d1d38664c582f90286ad226
Rishabh Raizada <rishabh.raizada@trilogy.com>
2022-10-08 19:48:32 +0000

    Removed unneccesary extra files

--- src/custom-components/inspector/panels/components/DejiPanel.oc.tsx
+++
@@ -1,7 +0,0 @@
-import React, { memo } from 'react'
-
-const DejiPanel = () => {
-  return <></>
-}
-
-export default memo(DejiPanel)
--- src/custom-components/inspector/panels/components/DjPanel.oc.tsx
+++
@@ -1,7 +0,0 @@
-import React, { memo } from 'react'
-
-const DjPanel = () => {
-  return <></>
-}
-
-export default memo(DjPanel)
