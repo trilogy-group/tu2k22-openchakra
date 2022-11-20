5ed284d015976f296ad96d118ebd8f051d8097b2
ayodeji-ti <ayodeji.odesile@trilogy.com>
2022-11-20 21:39:10 +0000

    fixed loop bug

--- src/components/editor/previews/LoopPreview.tsx
+++ src/components/editor/previews/LoopPreview.tsx
@@ -10,12 +10,30 @@ const LoopPreview: React.FC<{ component: IComponent }> = ({ component }) => {
   if (isOver) {
     props.bg = 'teal.50'
   }
+  const loopArray: number[] = []
+  const loopLength = Number(props.loopNumber)
+
+  const getArray = (length: number) => {
+    for (let i = 1; i <= length; i++) {
+      loopArray.push(i)
+    }
+  }
+
+  if (loopLength && loopLength > 0) {
+    getArray(loopLength)
+  }
 
   return (
     <Box pos="relative" ref={drop(ref)} {...props}>
-      {props.loopView ? (
+      {!props.loopView ? (
         <>
-          {[1,2,3,4].map(() =>
+          {component.children.map((key: string) => (
+            <ComponentPreview key={key} componentName={key} />
+          ))}
+        </>
+      ) : loopLength && loopLength > 0 ? (
+        <>
+          {loopArray.map(() =>
             component.children.map((key: string) => (
               <ComponentPreview key={key} componentName={key} />
             )),
@@ -23,9 +41,11 @@ const LoopPreview: React.FC<{ component: IComponent }> = ({ component }) => {
         </>
       ) : (
         <>
-          {component.children.map((key: string) => (
-            <ComponentPreview key={key} componentName={key} />
-          ))}
+          {[1, 2, 3, 4].map(() =>
+            component.children.map((key: string) => (
+              <ComponentPreview key={key} componentName={key} />
+            )),
+          )}
         </>
       )}
     </Box>
--- src/components/inspector/panels/components/LoopPanel.tsx
+++ src/components/inspector/panels/components/LoopPanel.tsx
@@ -1,6 +1,14 @@
 import React, { memo } from 'react'
 import SwitchControl from '~components/inspector/controls/SwitchControl'
+import TextControl from '~components/inspector/controls/TextControl'
 
-const LoopPanel = () => <SwitchControl label="Looped View" name="loopView" />
+const LoopPanel = () => {
+  return (
+    <>
+      <SwitchControl label="Looped View" name="loopView" />
+      <TextControl label="No of loops" name="loopNumber" />
+    </>
+  )
+}
 
 export default memo(LoopPanel)
