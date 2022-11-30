29d5719c66cee62c6da050a5e0beed323a2f8586
ayodeji-ti <ayodeji.odesile@trilogy.com>
2022-11-30 19:47:18 +0000

    update modal panel

--- src/components/inspector/panels/components/ModalPanel.tsx
+++ src/components/inspector/panels/components/ModalPanel.tsx
@@ -3,16 +3,33 @@ import { useForm } from '~hooks/useForm'
 import usePropsSelector from '~hooks/usePropsSelector'
 import SizeControl from '~components/inspector/controls/SizeControl'
 import SwitchControl from '~components/inspector/controls/SwitchControl'
+import { Select } from '@chakra-ui/react'
+import FormControl from '~components/inspector/controls/FormControl'
 
 const ModalPanel = () => {
   const { setValueFromEvent } = useForm()
   const size = usePropsSelector('size')
+  const motionPreset = usePropsSelector('motionPreset')
 
   return (
     <>
       <SwitchControl label="Show Preview" name="showpreview" />
       <SwitchControl label="Open" name="isOpen" />
       <SwitchControl label="Centered" name="isCentered" />
+      <FormControl htmlFor="motionPreset" label="Transition">
+        <Select
+          id="motionPreset"
+          onChange={setValueFromEvent}
+          name="motionPreset"
+          size="sm"
+          value={motionPreset || ''}
+        >
+          <option>scale</option>
+          <option>slideInBottom</option>
+          <option>slideInRight</option>
+          <option>none</option>
+        </Select>
+      </FormControl>
 
       <SizeControl
         label="Size"
