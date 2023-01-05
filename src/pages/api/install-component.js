47b2df55818376c511758e2dfbf021328306c988
Rishabh Raizada <110235735+rishabh-ti@users.noreply.github.com>
2023-01-05 18:46:14 +0530

    Update install-component.js

--- src/pages/api/install-component.js
+++ src/pages/api/install-component.js
@@ -35,7 +35,6 @@ export default async function handler(req, res) {
       'utf-8',
     )
 
-    shell.exec(`rm pnpm-lock.yaml`)
     shell.exec(`pnpm i ${req.body.path}`)
 
     res.status(200).json(componentName)
